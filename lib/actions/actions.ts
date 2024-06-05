575fc6979ea48dfc20f2ae9021f01e363e33f933
ashraf taha <atahaegy2001@gmail.com>
2024-06-05 07:31:23 +0300

    add vercel link

--- lib/actions/actions.ts
+++ lib/actions/actions.ts
@@ -1,6 +1,6 @@
 
 export const getCollections = async () => {
-  const collectionsfetch = await fetch(`${process.env.NEXT_PUBLIC_API_KEY}/collections`)
+  const collectionsfetch = await fetch(`https://borcelle-admin-prr6eby9l-ashrafs-projects-17880677.vercel.app/api/collections`)
   // const collectionsfetch = await fetch(process.env.NEXT_PUBLIC_API_URL +'/collections')
   const collections = await collectionsfetch.json()
   return collections
