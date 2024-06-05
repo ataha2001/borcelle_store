2c931e51c7df0ee167c2efeab489097cbb5bb563
ashraf taha <atahaegy2001@gmail.com>
2024-06-05 07:05:00 +0300

    edit banner link

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div>
-    <Image src='/banner.png' alt="banner" width={2000} height={1000} className="w-screen" />
+    <Image src='/banner.png' alt="banner" width={2000} height={1000} className="w-screen" blurDataURL={'/banner.png'} />
     
     <Collections />
     <ProductsList />
